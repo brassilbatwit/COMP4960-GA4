2b3e80c44e0e2c77a40358c1df84d30abd314ef4
cornelltATWIT <90805254+cornelltATWIT@users.noreply.github.com>
2025-01-11 17:56:41 -0500

    Update Region3.java
    Potential outline with Switch Statement to receive items

--- Region3.java
+++ Region3.java
@@ -1,5 +1,47 @@
 import java.util.Scanner;
-
+//Author Tyler Cornell
 public class Region3 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Welcome Prompt for Entering room Three
+        System.out.println("Welcome to Room Three of the Garden!");
+        
+        // Prompt player with choices
+        System.out.println("Now that you have entered the room what would you like to do");
+        System.out.println("1. Acquire fresh tomatoes.");
+        System.out.println("2. Search nearby shed.");
+        System.out.println("3. Explore room 2");
+        System.out.println("4. Explore room 4");
+        System.out.println("Enter the number of your choice (1/2/3/4): "); 
+        
+        int FirstChoice = scanner.nextInt();
+        
+        String item = "";
+        //Based on the choice of the user display further options
+        switch (FirstChoice) {
+            case 1:
+                System.out.println("How many tomatoes would you like to pick?");
+                System.out.println("1. 3 tomatoes");
+                System.out.println("2. 6 tomatoes");
+                System.out.println("Enter your choice (1/2): ");
+                int choiceTomatoes = scanner.nextInt();
+                item = (choiceTomatoes == 1) ? "3 tomatoes" : "6 tomatoes";
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            default:
+                System.out.println("Invalid input.Please select an option 1-4");
+                break;
+        }
+        
+        // Show player the item they have received
+        if (!item.isEmpty()) {
+            System.out.println("Congratulations! You have recieved: " + item);
+        }
+    }
 }
