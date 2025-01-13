021f3b069bd6ae025e15d34913bf4816c7a8293c
brassilbatwit <113615066+brassilbatwit@users.noreply.github.com>
2025-01-12 21:21:18 -0500

    Added IO helpers & updated docs for Game class

--- Game.java
+++ Game.java
@@ -1,11 +1,26 @@
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.EnumSet;
 import java.util.Scanner;
 
 public class Game {
+    /**
+     * Text input stream.
+     */
+    public static final Scanner input = new Scanner(System.in);
+
+    /**
+     * Text output stream.
+     */
+    public static final PrintStream output = System.out;
+
     private static final AssetManager assetManager = new AssetManager();
 
+    /**
+     * Provides access to game assets.
+     * @return {@code AssetManager} for this game.
+     */
     public static AssetManager getAssets() {
         return assetManager;
     }
@@ -17,7 +32,7 @@ public class Game {
      * @param output Text output stream
      * @return One of the values in {@code options}
      */
-    public static CardinalDirection chooseDirection(EnumSet<CardinalDirection> options, Scanner input, PrintStream output) {
+    public static CardinalDirection chooseDirection(EnumSet<CardinalDirection> options) {
         String s = assetManager.getDialogue("directionPrompt");
         output.println(s);
         for (CardinalDirection option : options) {
@@ -35,5 +50,26 @@ public class Game {
         }
 
         return choice;
+    }
+
+    /**
+     * Clears the standard output for this process.
+     */
+    public static void clearOutput() {
+        try {
+            ProcessBuilder process = isWindows()
+                ? new ProcessBuilder("cmd", "/c", "cls")
+                : new ProcessBuilder("clear");
+            process.inheritIO().start().waitFor();
+        }
+        catch (IOException | InterruptedException ex) {}
+    }
+
+    /**
+     * Checks if this process is running on a Windows operating system.
+     * @return True if running on Windows, otherwise false.
+     */
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
     }
 }
