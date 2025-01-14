f05a2362dcf1dbb35c017b3748c4849afe43da08
Dalton JS Crawford <148571937+DJSC3400@users.noreply.github.com>
2025-01-13 21:18:41 -0500

    Update main.java
    Added a call to room two and an items hash map.

--- main.java
+++ main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Runs room system starting at room two.
+        HashMap<String, Integer> items = new HashMap<>(); // Dictionary to store items and their quantities
+        Region2.Room2(items);
+        
         try {
             AssetManager assets = Game.getAssets();
             assets.load(Paths.get(System.getProperty("user.dir")));
